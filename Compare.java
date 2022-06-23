b5d14decc01dcd213613ffb217b0b891173626fe
SurajMaddala <107826831+SurajMaddala@users.noreply.github.com>
2022-06-23 09:24:23 +0530

    Update Compare.java

--- Compare.java
+++ Compare.java
@@ -1,78 +1,47 @@
-package excel;
-
-import java.io.File;
-import java.io.FileInputStream;
+package utils;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
-public class Comparison 
+import java.util.ArrayList;
+import java.util.List;
+public class compareToFiles
 {
-	public static void main(String[] args) throws IOException, InvalidFormatException {
-		
-		File file1 = new File("data\\BankSide.xlsx");   //creating a new file instance  
-		File file2= new File("data\\userDetails.xlsx");
+public static void main(String[] args) throws IOException
+{
+BufferedReader reader1 = new BufferedReader(new FileReader("C:\\\\Users\\\\SURAMADD\\\\Desktop\\\\text1.txt"));
+BufferedReader reader2 = new BufferedReader(new FileReader("C:\\\\Users\\\\SURAMADD\\\\Desktop\\\\text2.txt"));
+String line1 = reader1.readLine();
+String line2 = reader2.readLine();
+List<String> matchList = new ArrayList<>();
+List<String> mismatchList = new ArrayList<>();
+boolean areEqual = true;
+int lineNum = 3;
+while (line1 != null || line2 != null)
+{
+if(line1 == null || line2 == null)
+{
+areEqual = false;
+}
+else if(! line1.equalsIgnoreCase(line2))
+{
+areEqual = false;
+}
+if(areEqual)
+{
+matchList.add("Line" + lineNum + " = " + line1);
+}
+else{
+mismatchList.add("Line" + lineNum + " in file1 = " + line1 + " and in file2 = " + line2);
+}
 
-		FileInputStream f1 = new FileInputStream(file1);   //obtaining bytes from the file  
-		FileInputStream f2 = new FileInputStream(file2);
+line1 = reader1.readLine();
+line2 = reader2.readLine();
+lineNum++;
+}
+reader1.close();
+reader2.close();
 
-		XSSFWorkbook wb1 = new XSSFWorkbook(f1);
-		XSSFWorkbook wb2 = new XSSFWorkbook(f2);
-
-
-		XSSFSheet s1=wb1.getSheet("bank");
-		XSSFSheet s2=wb2.getSheet("user");
-
-
-		int rowcount1=s1.getPhysicalNumberOfRows();
-		int  rowcount2=s2.getPhysicalNumberOfRows();
-
-
-		if( rowcount1== rowcount2) 
-		{
-
-			for(int i=0;i< rowcount1 ;i++)
-
-			{
-				for(int j=1 ;j < rowcount2 ;i++)
-				{
-
-					XSSFRow r1=s1.getRow(i);
-					XSSFRow r2=s2.getRow(i);
-
-
-					XSSFCell c1=r1.getCell(i);
-					XSSFCell c2=r2.getCell(i);
-
-					c1.setCellType(CellType.STRING);
-					String str1=c1.getStringCellValue();
-
-
-					c2.setCellType(CellType.STRING);
-					String str2=c2.getStringCellValue();
-
-					if(str1.equals(str2)) 
-					{
-						System.out.println(" Match    |    "+str1 +"  ==>  "+str2);
-						System.out.println("---------------------------------------------------------------------------------------------");
-						
-					}
-					else {
-						
-
-						System.out.println("Mismatch |    "+str1 +"  ==>  "+str2);
-						System.out.println("---------------------------------------------------------------------------------------------");
-					}
-				}
-			}
-		}
-
-
-	}
-
+System.out.println("matchList :\n"+matchList);
+System.out.println("\nmismatchList:\n"+mismatchList);
+}
 }
